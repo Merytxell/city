4cc3a5e4079780e5bf68922152d21664071231f7
Claire AVILA <avila.claire@gmail.com>
2024-01-09 13:18:50 +0100

    création première classe avec attributs villes

---
+++ src/fr/fms/city/City.java
@@ -0,0 +1,20 @@
+package fr.fms.city;
+
+public class City {
+	
+	//atributs
+	public String country;
+	public String city;
+	public int nbInhabitants;
+	
+	// constructeurs 
+	
+	public City (String country, String city, int nbInhabitants) {
+		this.country= country; 
+		this.city=city;
+		this.nbInhabitants= nbInhabitants;
+		
+	}
+}
+//ajouter première classe city avec nom de ville nb habitant et pays -ok
+//définir un constructeur - ok
---
+++ src/fr/fms/city/TestCity.java
@@ -0,0 +1,15 @@
+package fr.fms.city;
+
+public class TestCity {
+	
+	public static void main (String [] args){
+		City city1 = new City ("France","Toulouse",450000); 
+		System.out.println(city1);
+	
+		
+		
+	}
+}
+//ajouter une classe - ok
+//tester avec plusieurs villes 
+//ajouter 20 000 a l'attribut nbInhabitants'
