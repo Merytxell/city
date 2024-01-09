feed6e316072cd1eb52136def2003d93e2bbda5c
Claire AVILA <avila.claire@gmail.com>
2024-01-09 13:42:11 +0100

    ajout des villes

--- src/fr/fms/city/TestCity.java
+++ src/fr/fms/city/TestCity.java
@@ -3,10 +3,13 @@ package fr.fms.city;
 public class TestCity {
 	
 	public static void main (String [] args){
-		City city = new City ("France","Toulouse",450000); 
+		City city = new City ("France","Toulouse",450000);
+		City city1 = new City ("Espagne", "Murcia", 447000);
+		City city2 = new City ("Japon", "Nara", 360310);
 		
 		city.display();
-		
+		city1.display();
+		city2.display();
 		
 		
 	}
