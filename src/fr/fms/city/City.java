93b9e0a16a04c828fb5fdc168ec17c96503adc9d
Claire AVILA <avila.claire@gmail.com>
2024-01-10 12:52:12 +0100

    exo 1.2 terminé

--- src/fr/fms/city/City.java
+++ src/fr/fms/city/City.java
@@ -7,16 +7,20 @@ public class City {
 	public String city;
 	private double nbInhabitants;
 	
+	public static final double Min_City_nbInhabitants = 450000;
 	
-	
+	//accesseur
 	public double  getNbInhabitants (){
 		return nbInhabitants;
 	}
-	
+	//accesseur
 	public void setInhabitants (double i) {
-		this.nbInhabitants = i;
 		
-		
+		if (i <= 0) {
+			System.out.println("il est impossible qu'une ville dispose de moins de 0 habitants !");
+		}else {
+			this.nbInhabitants = Min_City_nbInhabitants;
+	
 	
 		/*
 		 * public City (String country, String city, int nbInhabitants) { this.country=
@@ -26,13 +30,14 @@ public class City {
 
 	
 	//methodes
-		public void display() {
+		/*public void display() {
 			System.out.println("pays : " + this.country + "\n" + "ville :"  + this.city + "\n" + "nombre d'habitants : " + this.nbInhabitants); 
-			
+			*/
 	}
 							
 }
 
 
 //ajouter première classe city avec nom de ville nb habitant et pays -ok
-//définir un constructeur - 
+//définir un constructeur - ok
+//définir des accesseurs permettant à user de modifier le nombre de personne dans uen ville -ok
--- src/fr/fms/city/TestCity.java
+++ src/fr/fms/city/TestCity.java
@@ -1,8 +1,12 @@
 package fr.fms.city;
 
+import java.util.Scanner;
+
 public class TestCity {
 	
 	public static void main (String [] args){
+		
+		
 		City toulouse = new City ();
 		/*
 		 * City city1 = new City ("Espagne", "Murcia", 447000); City city2 = new City
@@ -16,7 +20,9 @@ public class TestCity {
 		toulouse.city ="Toulouse";
 		toulouse.setInhabitants(-200);
 		
+		
 		System.out.println(toulouse.country + " " + toulouse.city + " " + toulouse.getNbInhabitants()); 
+		
 		
 		/*toulouse.nbInhabitants += 20000;	
 		toulouse.display();*/
@@ -26,3 +32,5 @@ public class TestCity {
 //ajouter une classe - ok
 //tester avec plusieurs villes - ok
 //ajouter 20 000 a l'attribut nbInhabitants - ok
+//empêcher l'user de mettre -200 habitants en l'informant avec un message - ok
+//permettre que l'user puisse décrémenter une population
