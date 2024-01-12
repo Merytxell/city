9f48f8ff7251d0bc2c4de2bf799b8d5d244c5551
Claire AVILA <avila.claire@gmail.com>
2024-01-12 15:21:00 +0100

    fin exo 2

--- src/fr/fms/people/TestPeople.java
+++ src/fr/fms/people/TestPeople.java
@@ -4,28 +4,30 @@ import java.util.ArrayList;
 
 
 public class TestPeople {
-	
-	 
+
+
 
 
 	public static void main (String[] args) {
-		
+
 		ArrayList<People>ListPeople=new ArrayList<>();
-		
+
 
 		ListPeople.add(new People("Macron","Manu",43,"Paris","Amiens","France"));
 		ListPeople.add(new People("Sarkozy","Nicolas", 66, "Paris", "Paris","France",20)); 
 		ListPeople.add(new People("Jonhson", "Boris",56, "London", "New York" , "Etats-Unis"));
-		ListPeople.add(new People("Noah","Yannick",63, "Etoudi","Sedan","Cameroun"));
-		ListPeople.add(new People("Kravitz" ,"Lenny", 56 ,"Paris","New-York","USA"));
+		ListPeople.add(new People("Noah","Yannick",63, "Sedan","Etoudi","Cameroun"));
+		ListPeople.add(new People("Kravitz" ,"Lenny", 56 ,"Paris","New-York","France"));
 		ListPeople.add(new People("Lawrence","Jennifer",30,"Lousiville", "Indian Hlls", "USA"));
 		
-		
-		
-		
-		
-	
-	
-	
-}
+
+		for (People person :ListPeople) {
+			if (person.getBornCity()==("Paris") || person.getCountryOfResidence()==("France") ||person.getAddress()==("Paris"))
+			{
+				System.out.println(person);
+				//System.out.println(person.getCountryOfResidence());
+			}
+
+		}
+	}
 }
